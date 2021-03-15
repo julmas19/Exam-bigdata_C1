2702716e2d7cdab446d7e90849c700071c2700bd
julmas19 <61039738+julmas19@users.noreply.github.com>
2021-03-15 13:28:53 -0500

    Update lambda_funtion.py

--- lambda_funtion.py
+++ lambda_funtion.py
@@ -17,7 +17,7 @@ def lambda_handler(event, context):
     s3 = boto3.client('s3')
     cont = 0
     if(ext_type == 'txt'):
-        s3.download_file("julianbucket19", key, new_ubi)
+        s3.download_file("julianbucket22", key, new_ubi)
         line = open(new_ubi, 'r')
         lineas = line.readlines()
         line.close()
@@ -37,12 +37,19 @@ def lambda_handler(event, context):
         doc = lineas[0].split(',')[0].split('-')
         meses = ['enero','febrero','marzo','abril','mayo','junio','julio','agosto','septiembre','octubre','noviembre','diciembre']
         mes=meses[int(doc[1])-1]
-        ubiex = "/bucketexam1bd/stocks/company={}/year={}/month={}/day={}/".format(key_name,doc[0],mes,doc[2])
+        
+        ubiex = "stocks/company={}/year={}/month={}/day={}/".format(key_name,doc[0],mes,doc[2])
         sintaxkey = 'stocks/company={}/year={}/month={}/day={}/{}'.format(key_name,doc[0],mes,doc[2],new_name)
         #s3.put_object(Bucket='bucketexam1bd', Key = sintaxkey)
-        
-        s3.upload_file(new_ubi,"bucketexam1bd",sintaxkey)
-        bucket_name = "julianbucket21"
+        s3d = boto3.resource('s3')
+        s3.put_object(Bucket='bucketexambd', Key = sintaxkey)
+        objects_to_delete = s3d.meta.client.list_objects(Bucket="bucketexambd", Prefix=ubiex)
+        delete_keys = {'Objects' : []}
+        delete_keys['Objects'] = [{'Key' : k} for k in [obj['Key'] for obj in objects_to_delete.get('Contents', [])]]
+        s3d.meta.client.delete_objects(Bucket="bucketexambd", Delete=delete_keys)
+        #subida de archivo
+        s3.upload_file(new_ubi,"bucketexambd",sintaxkey)
+        bucket_name = "athenalogsexam"
         client = boto3.client("athena")
         config = {
             "OutputLocation": "s3://" + bucket_name + "/",
